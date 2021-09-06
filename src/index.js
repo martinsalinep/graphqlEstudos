ad2612c36d924bf50bd30b68efa576c6d7978e30
martinsalinep <alinemartins@hotmail.com.br>
2021-09-06 13:10:48 -0300

    primeiras queries com graphql

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('oi');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+      hi: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => {
+        return 'hello again';
+      },
+      hi: () => {
+        return 'hi';
+      },
+    },
+  },
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`Server listening on url ${url}`);
+});
