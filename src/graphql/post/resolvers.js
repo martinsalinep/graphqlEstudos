611f50ac1b03569b45025375a09622189a3570eb
martinsalinep <alinemartins@hotmail.com.br>
2021-09-07 15:38:57 -0300

    union type

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -7,6 +7,12 @@ const posts = async (_, { input }, { getPosts }) => {
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts('/' + id);
   const post = await response.json();
+  if (typeof post.id === 'undefined') {
+    return {
+      statusCode: 404,
+      message: 'Post not found',
+    };
+  }
   return post;
 };
 
@@ -14,5 +20,12 @@ export const postResolvers = {
   Query: {
     posts,
     post,
+  },
+  PostResult: {
+    __resolveType: (obj) => {
+      if (typeof obj.statusCode !== undefined) return 'PostNotFoundError';
+      if (typeof obj.id !== undefined) return 'Post';
+      return null;
+    },
   },
 };
